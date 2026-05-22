bbc2a5add21bd99b9265e2381beedb430f4a4a8b
ChunyuPCY <18896534787@163.com>
2026-05-22 11:35:01 +0800

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -74,7 +74,7 @@ function PackUtils.get_root(name)
   name = PackUtils.get_name(name)
   local paths = vim.api.nvim_get_runtime_file('pack/*/*/' .. name, true)
   if #paths > 0 then return paths[1] end
-  local glob = vim.fn.globpath(vim.o.packpath, 'pack/*/*/' .. name, 0, 1)
+  local glob = vim.fn.globpath(vim.o.packpath, 'pack/*/*/' .. name, false, true)
   return glob[1] or nil
 end
 

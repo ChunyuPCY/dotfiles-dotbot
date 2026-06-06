4f0b267c39284458a22c1127a780544a72da7206
ChunyuPCY <18896534787@163.com>
2026-06-06 18:47:06 +0800

    update

---
+++ lazyvim/lua/plugins/lsp.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ["*"] = {
+          keys = {
+            -- 覆盖原有的 K 键映射
+            {
+              "K",
+              function()
+                -- 关键：在调用 hover 时传入 border 参数
+                -- vim.lsp.buf.hover({
+                --   offset_x = 4,
+                --   offset_y = 4,
+                --   border = "rounded",
+                -- })
+                vim.lsp.buf.hover()
+              end,
+              desc = "Hover (with border)",
+            },
+          },
+        },
+      },
+    },
+  },
+}
---
+++ lazyvim/lua/plugins/noice.lua
@@ -0,0 +1,8 @@
+return {
+  "noice.nvim",
+  opts = {
+    presets = {
+      lsp_doc_border = true, -- Add border to floating window
+    },
+  },
+}
